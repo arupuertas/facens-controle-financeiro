7c005deba5100f3be8e299a12a2e8672f3ebf467
Aruã Puertas <58257584+arupuertas@users.noreply.github.com>
2024-11-10 15:00:27 +0000

    fix: main.py and text

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ class Configurations():
     def __init__(self):
         self.__file_output = './out/transactions.txt'
         
-    #Quem instaciar a file configuraton pode ver mas não pode editar
+    #Quem instanciar a file configuration pode ver mas não pode editar
     @property
     def file_output(self):
         return self.__file_output
